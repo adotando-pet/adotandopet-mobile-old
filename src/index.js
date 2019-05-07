8cd981584f94c6161adcfb34dbdae06b41af9662
Claudio Orlandi <cso.junior1996@gmail.com>
2019-05-07 00:40:44 -0300

    Configurando Redux Persist
    Com essa configuração será possível manter os dados do usuário mesmo com o aplicativo sendo fechado e reaberto!

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 import '~/config/ReactotronConfig';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 import Routes from '~/navigator';
 
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <PersistGate loading={null} persistor={persistor}>
+      <Routes />
+    </PersistGate>
   </Provider>
 );
 
