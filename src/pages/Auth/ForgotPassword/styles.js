4f0c60452fef1ad58d39dd885b93eb0d36451d87
Claudio Orlandi <cso.junior1996@gmail.com>
2019-05-10 20:11:26 -0300

    Create more screens

--- src/pages/Auth/ForgotPassword/styles.js
+++ src/pages/Auth/ForgotPassword/styles.js
@@ -1,6 +1,21 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import { Platform } from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+import { colors, metrics, fonts } from '~/styles';
+
+export const Container = styled(LinearGradient).attrs({
+  colors: [colors.secundary, colors.primary],
+})`
   flex: 1;
-  background-color: #f00;
+`;
+
+export const ContentContainer = styled.KeyboardAvoidingView.attrs({
+  behavior: Platform.OS === 'ios' ? 'padding' : null,
+})`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
