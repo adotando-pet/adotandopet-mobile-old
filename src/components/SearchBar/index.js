eba206cae87e4dc72f003bf277ffa27266ca8c78
davimattos <davimattosdev@gmail.com>
2019-11-03 14:56:39 -0300

    refatora para comp baseado em funcao (hooks)

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState } from 'react';
 
 import {
   Container,
@@ -10,30 +10,24 @@ import {
   DetailedFilters,
 } from './styles';
 
-export default class SearchBar extends Component {
-  state = {
-    filtersVisible: false,
-  };
+export default function SearchBar() {
+  const [filtersVisible, setFiltersVisible] = useState(false);
 
-  changeFiltersVisibility = () => {
-    const { filtersVisible } = this.state;
-    this.setState({ filtersVisible: !filtersVisible });
-  };
-
-  render() {
-    const { filtersVisible } = this.state;
-    return (
-      <Fragment>
-        <Container>
-          <Input placeholder="O que você está procurando?" />
-          <SearchIcon name="search" />
-        </Container>
-        <FiltersContainer onPress={this.changeFiltersVisibility}>
-          <ExpandIcon />
-          <FiltersText>Filtros</FiltersText>
-        </FiltersContainer>
-        {filtersVisible && <DetailedFilters />}
-      </Fragment>
-    );
+  function changeFiltersVisibility() {
+    setFiltersVisible(!filtersVisible);
   }
+
+  return (
+    <>
+      <Container>
+        <Input placeholder="O que você está procurando?" />
+        <SearchIcon name="search" />
+      </Container>
+      <FiltersContainer onPress={changeFiltersVisibility}>
+        <ExpandIcon />
+        <FiltersText>Filtros</FiltersText>
+      </FiltersContainer>
+      {filtersVisible && <DetailedFilters />}
+    </>
+  );
 }
